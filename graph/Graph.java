730c5cc2d9d5cb4a7df5c2138feb0773df96b924
holeguma <hmyyholeguma@gmail.com>
2020-10-29 02:26:19 +0900

    Introduce online-judge-verify-helper

---
+++ graph/Graph.java
@@ -0,0 +1,9 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class Graph extends ArrayList<long[]> {
+
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ template/Main.java
@@ -0,0 +1,25 @@
+package template;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class Main {
+
+	static InputReader ir;
+	static PrintWriter out;
+
+	static void solve() {
+		// Place your solution here.
+	}
+
+	public static void main(String[] args) {
+		ir = new InputReader(System.in);
+		out = new PrintWriter(System.out);
+		solve();
+		out.flush();
+	}
+
+	static void tr(Object... o) {
+		System.out.println(Arrays.deepToString(o));
+	}
+}
